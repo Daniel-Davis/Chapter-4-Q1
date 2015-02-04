ad64eb2b9927b64465a68e4103583190c32024d7
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-04 10:08:57 -0800

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,5 +1,13 @@
 // Daniel Davis, sum, sums of an array//
 
-funtion sum(array) {
+function sum(array) {
+
+  var total=0;
+  
+  while (var i=0, i<=array.length, i++) {
+    total= array[i] + total
+  }// end of while loop
+
+  return total;
 
 }// end of function
